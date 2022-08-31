ae48453d5c610539c12bd7065f8cf7b6d8a4e127
LeanAgLotfi <106394110+LeanAgLotfi@users.noreply.github.com>
2022-08-31 10:53:17 -0300

    nuevo main.js arreglo de carrito

--- main.js
+++ main.js
@@ -1,259 +1,218 @@
-/*const ulFiltro = document.getElementById('filtros');
+const Clickbutton = document.querySelectorAll('.button');
+const tbody = document.querySelector('.tbody')
+let carrito = []
 
-//ARRAY DE OBJETOS
-const arrayDelibros = []
 
-const arrayLibros = [
-    {   nombre: 'don quijote',
+Clickbutton.forEach(btn =>{
+    btn.addEventListener('click', addCarritoItem)
+})
+
+function addCarritoItem(e){
+    const button = e.target
+    const item = button.closest('.card')
+    const itemTitle = item.querySelector('.card-title').textContent;
+    const itemPrice = item.querySelector('.precio').textContent;
+    const itemImg = item.querySelector('.card-img-top').src;
+
+    const newItem = {
+        title : itemTitle,
+        precio :itemPrice,
+        img : itemImg,
+        cantidad : 1,
+    }
+
+    addItemCarrito(newItem)
+}
+
+function addItemCarrito(newItem){
+
+    const InputElemento = tbody.getElementsByClassName('input__elemento')
+
+for(let i =0; i < carrito.length; i++){
+    if(carrito[i].title.trim() === newItem.title.trim()){
+      carrito[i].cantidad ++;
+        const InputValue = InputElemento[i]
+        InputValue.value++;
+        CarritoTotal()
+      return null;
+    }
+}
+
+    carrito.push(newItem)
+    renderCarrito()
+}
+
+function renderCarrito(){
+    tbody.innerHTML = ''
+    carrito.map(item  =>{
+        const tr = document.createElement('tr')
+        tr.classList.add('ItemCarrito')
+        const Content = `
+        <th scope="row">1</th>
+        <td class="table__producto">
+            <img src=${item.img} alt="">
+            <h6 class="title">${item.title}</h6>
+        </td>
+        <td class="table__precio"><p>${item.precio}</p></td>
+        <td class="table__cantidad">
+            <input type="number" min="1" value="${item.cantidad}" class="input__elemento">
+            <button class="delete btn btn-danger">X</button>
+        </td>`
+
+        tr.innerHTML = Content;
+        tbody.appendChild(tr)
+
+        tr.querySelector('.delete').addEventListener('click', removeItemCarrito)
+        tr.querySelector('.input__elemento').addEventListener('change', sumaCantidad)
+    })
+    CarritoTotal()
+}
+
+
+function CarritoTotal(){
+let Total = 0;
+const itemCartTotal = document.querySelector('.itemCartTotal')
+carrito.forEach((item) =>{
+    const precio = Number(item.precio.replace("$", ''))
+    Total = Total + precio*item.cantidad
+})
+
+itemCartTotal.innerHTML = `Total $${Total}`
+esLocalStorage()
+
+}
+
+function removeItemCarrito(e){
+    const buttonDelete = e.target
+    const tr = buttonDelete.closest(".ItemCarrito")
+    const title = tr.querySelector('.title').textContent;
+    for(let i=0; i<carrito.length ; i++){
+  
+      if(carrito[i].title.trim() === title.trim()){
+        carrito.splice(i, 1)
+      }
+    }
+    tr.remove()
+    CarritoTotal()
+}
+
+function sumaCantidad(e){
+    const suma = e.target
+    const tr = suma.closest(".ItemCarrito")
+    const title = tr.querySelector('.title').textContent;
+    carrito.forEach(item =>{
+        if(item.title.trim() === title){
+            suma.value < 1 ? (suma.value = 1) : suma.value;
+            item.cantidad = suma.value;
+            CarritoTotal()
+        }
+    })
+
+}
+
+function esLocalStorage(){
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+}
+
+
+window.onload = function(){
+    const storage = JSON.parse(localStorage.getItem('carrito'));
+    if(storage){
+        carrito = storage;
+        renderCarrito()
+    }
+}
+
+const stockProductos= 
+[
+    {   nombre: ' don quijote',
         precio:200,
+        desc:'Las aventuras de Don quijote y Sancho panza en una encrucijada por la mancha',
+        genero:'Aventura',
+        img:'./imagenes/libro-1-array.png',
         id:1,
     },
 
-    {   nombre: 'moby dick', 
+    {   nombre: ' moby dick', 
         precio:120,
+        desc:'Una historia de marinos en busca del tesoro mas grande, capturar a "la gran ballena blanca"',
+        genero:'Aventura',
+        img:'./imagenes/libro-2-array.png',
         id:2
     },
 
-    {   nombre: 'fantasma de la opera',
+    {   nombre: ' fantasma de la opera',
         precio:100,
+        desc:'Un hombre atormentado cuyo rostro, deformado de nacimiento, le otorgaba el aspecto de una verdadera aparición en el subterraneo de la opera',
+        genero:'Terror',
+        img:'./imagenes/libro-3-array.png',
+        cantidad:1,
         id:3,
     },
 
-    {   nombre: 'it', 
+    {   nombre: ' it', 
         precio:700,
+        desc:'El payaso maligno venido del espacio solo para aterrorizar y deborar',
+        genero:'Terror',
+        img:'./imagenes/libro-4-array.png',
+        cantidad:1,
         id:4,
     },
 
-    {   nombre: 'harry potter',
+    {   nombre: ' harry potter',
         precio:800,
+        desc:'La historia del gran mago Harry Potter y sus aventuras por Howards',
+        genero:'Fantasia',
+        img:'./imagenes/libro-5-array.png',
+        cantidad:1,
         id:5,
     },
 
-    {   nombre: 'el señor de los anillos',
+    {   nombre: ' el señor de los anillos',
         precio:500,
+        desc:'La encrucijada mas grande y la posible guerra por el poder del anillo y su deseo de poseerlo!',
+        genero:'Fantasia',
+        img:'./imagenes/libro-6-array.png',
+        cantidad:1,
         id:6,
     },
 
-    {   nombre: 'orgullo y prejuicio', 
+    {   nombre: ' orgullo y prejuicio', 
         precio:250,
+        desc:'Narra las aventuras y desventuras amorosas de las hermanas Bennet, centrándose en el personaje de Elizabeth',
+        genero:'Romance',
+        img:'./imagenes/libro-7-array.png',
         id:7,
     },
 
-    {   nombre: 'bajo la misma estrella', 
+    {   nombre: ' bajo la misma estrella', 
         precio:120,
+        desc:' Una adolescente de 16 años de edad llamada Hazel Grace Lancaster, quien padece cáncer de tiroides, se enamora de un ex jugador de basquet que reside en el mismo hospital',
+        genero:'Romance',
+        img:'./imagenes/libro-8-array.png',
         id:8,
     },
 
-    {   nombre: 'crimen de la calle morgue', 
+    {   nombre: ' crimen de la calle morgue', 
         precio:400,
+        desc:'Un joven francés le llama la atención un informe periodístico sobre un misterioso doble asesinato en París. Junto con un amigo, estudia todos los testimonios y examina la escena del crimen.',
+        genero:'Policial',
+        img:'./imagenes/libro-9-array.png',
         id:9,
     },
 
     {   nombre: 'sherlock holmes', 
         precio:250,
+        desc:'Las Grandes aventuras de Sherlock y Watson resolviendo misterios por las calles de Londres',
+        genero:'Policial',
+        img:'./imagenes/libro-10-array.png',
         id: 10,
-    },
-]
-
-const libros = ['don quijote', 'moby dick', 'fantasma de la opera', 'it', 'harry potter', 'el señor de los anillos',  'orgullo y prejuicio', 'bajo la misma estrella', 'crimen de la calle morgue', 'sherlock holmes']
-
-
-
-const fragment = document.createDocumentFragment()
-
-//-------------------------------------------------
-libros.forEach( (libro) => {
-    const li = document.createElement('li')
-    li.textContent = libro
-    fragment.appendChild(li)
-})
-
-ulFiltro.appendChild(fragment);
-//-------------------------------------------------
-
-
-const contenido = document.querySelector(".contenido");
-const carritoDiv = document.getElementById("carrito");
-
-let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-
-function contenidoCarta() {
-    arrayLibros.forEach((prod) => {
-        contenido.innerHTML += `<div id="libros" style="padding: 20px; background-color:orange; border: 2px solid black;">
-        <h4>${prod.nombre}</h4>
-        <p>$${prod.precio}</p>
-        <button class="btnCarrito" id="btn-agregar${prod.id}">Agregar</button>
-        </div>`;
-    });
-}
-
-
-contenidoCarta();
-
-      
-      const containerDiv = document.getElementById("conteiner");
-      const carritoDiv = document.getElementById("carrito");
-      
-      let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-      
-      function crearCards() {
-        productos.forEach((prod) => {
-          containerDiv.innerHTML += `<div style="padding: 20px; background-color:orange; border: 2px solid black;">
-              <h4>${prod.nombre}</h4>
-              <p>$${prod.precio}</p>
-              <button class="btnCarrito" id="btn-agregar${prod.id}">Agregar</button>
-              </div>`;
-        });
-        agregarFuncionalidad();
-      }
-      
-      function agregarFuncionalidad() {
-        productos.forEach((prod) => {
-          document
-            .querySelector(`#btn-agregar${prod.id}`)
-            .addEventListener("click", () => {
-              console.log(prod);
-              agregarAlCarrito(prod);
-            });
-        });
-      }
-      
-      function agregarAlCarrito(prod) {
-        let existe = carrito.some((productoSome) => productoSome.id === prod.id);
-        if (existe === false) {
-          prod.cantidad = 1;
-          carrito.push(prod);
-        } else {
-          let prodFind = carrito.find((productoFind) => productoFind.id === prod.id);
-          prodFind.cantidad++;
-          //prod.cantidad++
-        }
-        //carrito.push(prod);
-        console.log(carrito);
-        renderizarCarrito();
-      }
-      
-      function renderizarCarrito() {
-        carritoDiv.innerHTML = "";
-        carrito.forEach((prod) => {
-          carritoDiv.innerHTML += `<div style="padding: 20px; background-color:green; border: 2px solid black;">
-              <h4>${prod.nombre}</h4>
-              <p>CANTIDAD: ${prod.cantidad}</p>
-              <button class="btnCarrito" id="btn-borrar${prod.id}">Borrar</button>
-              <button class="btnCarrito" id="btn-menos${prod.id}">-</button>
-              </div>`;
-        });
-        localStorage.setItem("carrito", JSON.stringify(carrito));
-        borrarProducto();
-      }
-      
-      function borrarProducto() {
-        carrito.forEach((prod) => {
-          document
-            .querySelector(`#btn-borrar${prod.id}`)
-            .addEventListener("click", () => {
-              carrito = carrito.filter(
-                (productoFilter) => productoFilter.id !== prod.id
-              );
-              renderizarCarrito();
-            });
-        });
-      }
-      
-      crearCards();
-      renderizarCarrito();
-
-
-
-
-
-
-
-let usuario = prompt("Ingrese un Nombre:")
-
-function ingresarUsuario() {
-    
-    alert('El usuario ingresado es: '+ usuario)
-}
-ingresarUsuario()
-
-function saludo() {
-    alert('Bienvenido '+ usuario)
-}
-
-function usuarioCorrecto(){
-    alert('Es correcto su nombre de usuario?')
-        let decision = prompt("si o no")
-
-    while (decision == "no"){
-        let pregunta = prompt("Cual es tu nombre?:")
-        alert("Bienvenido "+ pregunta)
-        break;
     }
-    if(decision == "si"){
-        saludo()
-    }
-}
+];
 
-usuarioCorrecto()
-//ARRAY
-const arrayProductos = ['don quijote', 'moby dick', 'fantasma de la opera', 'it', 'harry potter', 'el señor de los anillos',  'orgullo y prejuicio', 'bajo la misma estrella', 'crimen de la calle morgue', 'sherlock holmes']
-
-//CLASS
-class Producto {
-    constructor(nombre, precio){
-        this.nombre = nombre.toUpperCase();
-        this.precio = parseFloat(precio);
-        
-    }
-    sumaIva(){
-        this.precio = this.precio * 1.21;
-    }
-}
-//OBJETOS
-const pruducto1 = new Producto("don quijote", "300");
-const pruducto2 = new Producto("harry potter", "400");
-const pruducto3 = new Producto("fantasma de la opera", "150");
-const pruducto4 = new Producto("sherlok holmes", "100");
-console.log(pruducto1.sumaIva());
-console.log(pruducto2.sumaIva());
-console.log(pruducto3.sumaIva());
-
-console.table(arrayProductos);
-
-//FOREACH
-arrayProductos.forEach( (books)=>{
-    console.log(books);
-})
-
-//MAP
-const biblioteca = [
-    {nombre: 'don quijote', precio:200},
-    {nombre: 'moby dick', precio:120},
-    {nombre: 'fantasma de la opera', precio:100},
-    {nombre: 'it', precio:700},
-    {nombre: 'harry potter', precio:800},
-    {nombre: 'el señor de los anillos', precio:500},
-    {nombre: 'orgullo y prejuicio', precio:250},
-    {nombre: 'bajo la misma estrella', precio:120},
-    {nombre: 'crimen de la calle morgue', precio:400},
-    {nombre: 'sherlock holmes', precio:250},
-]
-
-const libros = biblioteca.map((el) => el.libros)
-console.log(libros);
-
-const actualizado = biblioteca.map((el) => { 
-    return {
-        nombre: el.nombre,
-        precio: el.precio * 1.25
-    }
-})
-
-console.log(actualizado);
-
+const nuevoArray = ['don quijote', 'moby dick', 'fantasma de la opera', 'it', 'harry potter', 'el señor de los anillos', 'orgullo y prejuicio', 'bajo la misma estrella', 'crimen de la calle morgue', 'sherlock holmes'];
+// const nombresLibros = document.getElementsByClassName('nombreLibro');
 let counter = 1;
-
 setInterval(function(){
     document.getElementById('radio' + counter).checked = true;
     counter++;
@@ -261,17 +220,39 @@ setInterval(function(){
         counter = 1; 
     }
 }, 5000);
-
 function verificar(){
     for(let i = 0; i < 20; i++)
     document.getElementById('btn$')
     alert(`seleccionaste ${i} de elementos`)
 }
 
-*/
+function algo(){
+    stockProductos.forEach(elemento => {
+        document.getElementsByClassName("nombreLibro").innerHTML = `${elemento.nombre}`
+    });
+}
+ algo();
+/*const productoCarrito = document.querySelector("#cabeza");
+let cartita = document.getElementById('cartita')
 
 
+function stock(){
+    stockProductos.forEach((productos) => {
+       cartita.innerHTML += 
+   `<div class="col d-flex justify-content-center-ab-4">
+       <div class="card shadow mb-1 bg-warning rounded" style="width: 20rem;">
+           <h5 class="card-title pt-2 text-center text-dark">${productos.nombre}</h5>
+           <img src="./imagenes/libro-1-array.png" class="card-img-top" alt="...">
+           <div class="card-body">
+             <p class="card-text text-dark-50 description">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+             <h5 class="text-dark">Precio: <span class="precio">$ 5</span></h5>
+             <div class="d-grid gap-2">
+               <button class="btn btn-primary button">Añadir al Carrito</button>
+             </div>
+           </div>
+         </div>
+   </div>`
+    });
+}
 
-
-
-
+stock();*/
